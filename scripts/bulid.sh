afd79f05fe17710ac04354013571553871ae649c
horrordor <cccccc2002@outlook.com>
2022-12-20 16:51:09 +0800

    clean up

--- scripts/bulid.sh
+++ scripts/bulid.sh
@@ -7,6 +7,6 @@ OUTPUT_BRANCH="docs_output"
 CN_OPTIONS="--prefix ${CN_SITE} -b ${OUTPUT_BRANCH}"
 
 mike delete --all ${CN_OPTIONS}
-git checkout main
-mike deploy main ${LATEST_VERSION} -u ${CN_OPTIONS}
+git checkout master
+mike deploy master ${LATEST_VERSION} -u ${CN_OPTIONS}
 mike set-default ${LATEST_VERSION} ${CN_OPTIONS}
