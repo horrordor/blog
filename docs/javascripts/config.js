95116f465a27052c7e10476c9c386b0b66be4f2a
horrordor <cccccc2002@outlook.com>
2023-10-26 12:12:05 +0800

    fuck

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -22,3 +22,12 @@ $('link').each(function () {
     console.log($replace);
     $(this).attr('href', $replace);
 });
+
+
+$('link').each(function () {
+    var $intial = $(this).attr('href'),
+        $replace = $intial.replace('https://fonts.gstatic.com', 'https://fonts.gstatic.font.im');
+    console.log($intial);
+    console.log($replace);
+    $(this).attr('href', $replace);
+});
